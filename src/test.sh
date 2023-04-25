cc219689fe9e4130d7d7e712a34526433c8b8651
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2023-04-25 14:31:35 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -1,3 +1,9 @@
-echo "Before: FOO is set to ${FOO}"
-export FOO="bar"
-echo "After: FOO is set to ${FOO}"
+function inline_link {
+  LINK=$(printf "url='%s'" "$1")
+
+  if [ $# -gt 1 ]; then
+    LINK=$(printf "$LINK;content='%s'" "$2")
+  fi
+
+  printf '\033]1339;%s\a\n' "$LINK"
+}
