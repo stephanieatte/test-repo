56b897248a30e31123b45f07fa2021049d767aa1
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2023-05-16 03:07:04 +1000

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-function inline_link {
-  LINK=$(printf "url='%s'" "$1")
-
-  if [ $# -gt 1 ]; then
-    LINK=$(printf "$LINK;content='%s'" "$2")
-  fi
-
-  printf '\033]1339;%s\a\n' "$LINK"
-}
-
-inline_link
+buildkite-agent step update "label" "Tests for $PACKAGE" --step $BUILDKITE_STEP_ID
