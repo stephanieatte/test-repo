bc967c6c9023634ecf8402c7b445e928b27dd07a
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2023-05-24 10:34:40 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -1,1 +1,14 @@
-buildkite-agent step update "label" "Tests for $PACKAGE" --step $BUILDKITE_STEP_ID
+curl \
+  -X POST \
+  -H "Authorization: Token token=\"$BUILDKITE_ANALYTICS_TOKEN\"" \
+  -F "data=@junit.xml" \
+  -F "format=junit" \
+  -F "run_env[CI]=buildkite" \
+  -F "run_env[key]=$BUILDKITE_BUILD_ID" \
+  -F "run_env[url]=$BUILDKITE_BUILD_URL" \
+  -F "run_env[branch]=$BUILDKITE_BRANCH" \
+  -F "run_env[commit_sha]=$BUILDKITE_COMMIT" \
+  -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
+  -F "run_env[job_id]=$BUILDKITE_JOB_ID" \
+  -F "run_env[message]=$BUILDKITE_MESSAGE" \
+  https://analytics-api.buildkite.com/v1/uploads
