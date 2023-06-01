9b1313ed68881b5863397d76835ff45debd3cad4
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2023-06-01 15:19:07 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -1,14 +1,21 @@
-curl \
-  -X POST \
-  -H "Authorization: Token token=\"$BUILDKITE_ANALYTICS_TOKEN\"" \
-  -F "data=@junit.xml" \
-  -F "format=junit" \
-  -F "run_env[CI]=buildkite" \
-  -F "run_env[key]=$BUILDKITE_BUILD_ID" \
-  -F "run_env[url]=$BUILDKITE_BUILD_URL" \
-  -F "run_env[branch]=$BUILDKITE_BRANCH" \
-  -F "run_env[commit_sha]=$BUILDKITE_COMMIT" \
-  -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
-  -F "run_env[job_id]=$BUILDKITE_JOB_ID" \
-  -F "run_env[message]=$BUILDKITE_MESSAGE" \
-  https://analytics-api.buildkite.com/v1/uploads
+#!/bin/bash
+
+(
+  set -x
+
+  env | sort
+
+  uname -a
+)
+
+if [[ -x "$(command -v node)" ]]; then
+  ( set -x; node --version )
+else
+  echo "WARN: NodeJS not installed"
+fi
+
+if [[ -x "$(command -v yarn)" ]]; then
+  ( set -x; yarn --version )
+else
+  echo "WARN: Yarn not installed"
+fi
