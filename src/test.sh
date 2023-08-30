52f0325d5fe4a7ee03573c66265fbf0d2b466209
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2023-08-30 15:54:05 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -2,7 +2,7 @@
 set -x
 echo "hi"
 
- if [ "$BUILDKITE_RETRY_COUNT" -ne 0 ]; then
+ if [ "$BUILDKITE_RETRY_COUNT" -eq 0 ]; then
           echo "Then there is a retrial"
           buidkite-agent pipeline upload .buildkite/pipeline.yml
         fi
