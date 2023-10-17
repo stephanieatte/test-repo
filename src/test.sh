ad3d55d33940f9d18744b0ac038652fe90765eb4
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2023-10-17 10:13:13 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -9,15 +9,4 @@
 )
 
 echo "hi"
-
-if [[ -x "$(command -v node)" ]]; then
-  ( set -x; node --version )
-else
-  echo "WARN: NodeJS not installed"
-fi
-
-if [[ -x "$(command -v yarn)" ]]; then
-  ( set -x; yarn --version )
-else
-  echo "WARN: Yarn not installed"
-fi
+buildkite-agent meta-data set "con-group" "deploy"
