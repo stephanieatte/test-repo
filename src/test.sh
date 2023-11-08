3abd494710790597c9a078367caf52dea79a5821
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2023-11-08 12:10:58 -0700

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -1,20 +1,14 @@
-#!/bin/bash
-
-(
-  set -x
-
-
-)
-
-#!/bin/bash
-
-DEPLOYMENT_ENV="stage"
-
-buildkite-agent pipeline upload <<YAML
 steps:
-  - label: "start gate"
-    command: echo "---> starting concurrency gate"
-    concurrency_group: "gate/${DEPLOYMENT_ENV}"
-YAML
-
-exit -1
+  - label: ":golang: :package:"
+    commands: 
+       - "mkdir output"
+       - "cd output"
+       - "mkdir BOO_artifacts"
+       - "cd BOO_artifacts"
+       - "touch log.txt"
+    env:
+     JOB_NAME: "BOO"
+    artifact_paths: "output/\${JOB_NAME}_artifacts/*"
+    plugins:
+      - docker#v3.3.0:
+          image: "golang:1.11"
