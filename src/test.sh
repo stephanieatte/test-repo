15f1fbfd8cc3dbf74e398e7cfae4a235b6d1499b
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2023-11-16 11:56:14 -0700

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+   set -euo pipefail
+
+   # Set up a variable to hold the meta-data from your block step
+   RELEASE_NAME="$(buildkite-agent meta-data get "release-name")"
+
+   # Create a pipeline with your trigger step
+   PIPELINE="steps:
+     - trigger: \"deploy-pipeline\"
+       label: \"Trigger deploy\"
+       build:
+         meta_data:
+           release-name: $RELEASE_NAME
+   "
+
+   # Upload the new pipeline and add it to the current build
+   echo "$PIPELINE" | buildkite-agent pipeline upload
