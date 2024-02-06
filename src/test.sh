cb585eb4158708c78734415f920388e0e2054d36
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2024-02-06 12:59:01 -0700

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -4,3 +4,11 @@ set -euo pipefail
 # Export environment variables
 export fruit="apple"
 export flower="rose"
+
+
+entrypointd:
+  01-install-pre-exit-hook: |
+    #!/bin/bash
+    set -euo pipefail
+    echo "#!/bin/bash\nset -euo pipefail\ndocker images" > /var/buildkite/hooks/pre-exit
+    chmod +x /var/buildkite/hooks/pre-exit
