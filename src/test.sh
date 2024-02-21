069266235cf2e6dc485693d293e0f8b0ef03890b
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2024-02-21 11:34:57 -0700

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -12,5 +12,8 @@ random_number=$((RANDOM % 5))
 # Guess the user's preferred language
 echo "trying to guess your preferred language: ${programming_languages[$random_number]}"
 
-
-
+if [[ "${preferred_language} == ${programming_languages[$random_number]}" ]]; then
+     echo "You got it, its $preferred_language""
+else
+     echo "You missed it, its $preferred_language"
+fi
