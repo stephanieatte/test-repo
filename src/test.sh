9eef6a3986367f7d0a546f9666551989c2a01802
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2024-02-21 11:41:58 -0700

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -10,10 +10,12 @@ echo "Elements of myArray: ${programming_languages[@]}"
 random_number=$((RANDOM % 5))
 
 # Guess the user's preferred language
-echo "trying to guess your preferred language: ${programming_languages[$random_number]}"
+echo "We guess your preferred language is: ${programming_languages[$random_number]}"
+
 
 if [[ "${preferred_language}" == "${programming_languages[$random_number]}" ]]; then
     echo "Yaay, we guessed correct, it's ${preferred_language}"
 else
-    echo "We guessed ${programming_languages[$random_number] but your preferred language is ${preferred_language}"
+    echo "We guessed wrong, it's ${preferred_language}"
 fi
+
