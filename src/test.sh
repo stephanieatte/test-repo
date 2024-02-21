8c87b6f29352f4309c42c08e6e54dabf4bca27b5
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2024-02-21 11:12:52 -0700

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-set -euo pipefail
 
-myArray=("cat" "dog" "mouse" "frog")
+# Declare an array named 'myArray'
+myArray=(element1 element2 element3)
 
-for str in ${myArray[@]}; do
-  echo $str
-done
+# Print the entire array
+echo "Elements of myArray: ${myArray[@]}"
+
+# Access individual elements of the array
+echo "First element: ${myArray[0]}"
+echo "Second element: ${myArray[1]}"
+echo "Third element: ${myArray[2]}"
