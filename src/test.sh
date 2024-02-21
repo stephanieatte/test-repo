c6dfc7a9b8dc5195ca1d5cddc52558836fdb8c8e
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2024-02-21 11:25:23 -0700

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -6,16 +6,11 @@ programming_languages=('Bash' 'Python' 'JavaScript' 'Ruby', 'Java')
 # Print the entire array
 echo "Elements of myArray: ${programming_languages[@]}"
 
-
 # Generate a random number between 0 and 4
 random_number=$((RANDOM % 5))
 
-echo "Random number between 0 and 4: $random_number"
-
 # Guess the user's preferred language
 echo "trying to guess your preferred language: ${programming_languages[$random_number]}"
-echo "Second element: $programming_languages[1]}"
-echo "Third element: ${programming_languages[2]}"
 
 
 
