08080e9dd7d597152d5bf09dd1f2007d1a9fb4eb
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2024-02-21 11:20:35 -0700

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -7,10 +7,10 @@ programming_languages=('Bash' 'Python' 'JavaScript' 'Ruby', 'Java')
 echo "Elements of myArray: ${programming_languages[@]}"
 
 # Guess the user's preferred language
-echo "First element: ${programming_languages[shuf -i 0-4 -n 1]}"
+echo "trying: ${programming_languages[$RANDOM % ${#list[@]}]}"
 echo "Second element: $programming_languages[1]}"
 echo "Third element: ${programming_languages[2]}"
 
 
 
-
+echo ${programming_languages[$RANDOM % ${#list[@]} ]}
