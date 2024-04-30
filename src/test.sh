8138ce6407cbace0d1e9e71ef898d770dcbff94d
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2024-04-30 12:49:38 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -8,13 +8,13 @@ API_TOKEN="bkua_63fa9fec9d119f650e8b00a2b5f0c5c20036cc65"
 build_id="9053"
 cooldown_seconds=36  # 1 hour cooldown
 
-# Function to check the status of the build
-check_build_status() {
-    build_url="https://api.buildkite.com/v2/organizations/${ORG_NAME}/pipelines/${PIPELINE_NAME}/builds/${build_id}"
-    build_status=$(curl -s -X GET -H "Authorization: Bearer ${API_TOKEN}" "${build_url}")
-    echo "${build_status}"
-}
+build_url="https://api.buildkite.com/v2/organizations/${ORG_NAME}/pipelines/${PIPELINE_NAME}/builds/${build_id}"
+  
+response=$(curl -s -H "Authorization: Bearer ${API_TOKEN}" "${build_url}")
+echo "Raw JSON Response:"
+echo "${response}"
 
+# Parse JSON and extract state
+build_status=$(echo "${response}" | jq -r ".state")
 
-build_status=$(check_build_status)
 echo $build_status
