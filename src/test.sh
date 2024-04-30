5058691bb85205734d9097e8316c2a406274939e
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2024-04-30 13:40:36 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -5,7 +5,7 @@ ORG_NAME="atte-test-org-1"
 PIPELINE_NAME="first"
 API_TOKEN="bkua_63fa9fec9d119f650e8b00a2b5f0c5c20036cc65"
 
-build_id="your_build_id"
+build_id="9053"
 cooldown_seconds=3600  # 1 hour cooldown
 
 # Function to check the status of the build
