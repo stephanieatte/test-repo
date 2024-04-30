1022fc9fb9dbc14d8291167eff1e147110cc1e8e
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2024-04-30 12:51:21 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -11,10 +11,10 @@ cooldown_seconds=36  # 1 hour cooldown
 build_url="https://api.buildkite.com/v2/organizations/${ORG_NAME}/pipelines/${PIPELINE_NAME}/builds/${build_id}"
   
 response=$(curl -s -H "Authorization: Bearer ${API_TOKEN}" "${build_url}")
-echo "Raw JSON Response:"
-echo "${response}"
+#echo "Raw JSON Response:"
+#echo "${response}"
 
 # Parse JSON and extract state
-build_status=$(echo "${response}" | jq -r ".state")
+#build_status=$(echo "${response}" | jq -r ".state")
 
-echo $build_status
+#echo $build_status
