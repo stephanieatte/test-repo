ffea6d403e3dbd3dc274432377e45b417f027c3d
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2024-04-30 13:52:26 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -6,20 +6,30 @@ PIPELINE_NAME="first"
 API_TOKEN="bkua_63fa9fec9d119f650e8b00a2b5f0c5c20036cc65"
 
 build_id="9053"
-cooldown_seconds=3600  # 1 hour cooldown
+cooldown_seconds=60  # 1 hour cooldown
+
+wait_with_cooldown
 
 # Function to check the status of the build
 check_build_status() {
     build_url="https://api.buildkite.com/v2/organizations/$ORG_NAME/pipelines/$PIPELINE_NAME/builds/$build_id"
     build_status=$(curl -s -H "Authorization: Bearer $API_TOKEN" "$build_url" | jq -r '.state')
     echo "The url is $build_status"
+
+    if [ "${build_status}" == "failed" ]; then
+    echo "Build failed. Retrying after an hour..."
+    trigger_build
+    elif [ "${build_status}" == "passed" ]; then
+        echo "Build succeeded!"
+    else
   
 }
 
 # Function to trigger the build
 trigger_build() {
-    build_url="https://api.buildkite.com/v2/organizations/${ORG_NAME}/pipelines/${PIPELINE_NAME}/builds/rebuild"
-    curl -s -X PUT -H "Authorization: Bearer ${API_TOKEN}" "${build_url}"
+    build_url="https://api.buildkite.com/v2/organizations/$ORG_NAME/pipelines/$PIPELINE_NAME/builds/rebuild"
+    curl -s  -H "Authorization: Bearer $API_TOKEN"\
+     -X PUT "${build_url}"
 }
 
 # Wait function with a cooldown
@@ -29,12 +39,6 @@ wait_with_cooldown() {
 
 # Example usage
 build_status=$(check_build_status)
-if [ "${build_status}" == "failed" ]; then
-    echo "Build failed. Retrying after an hour..."
-    wait_with_cooldown
-    trigger_build
-elif [ "${build_status}" == "passed" ]; then
-    echo "Build succeeded!"
-else
+
     echo "Build status: ${build_status}."
 fi
