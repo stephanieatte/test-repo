090ad5e1535a7662a46f4bca6dc8ca10be1e03cd
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2024-04-30 12:55:01 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -6,8 +6,8 @@ PIPELINE_NAME="first"
 API_TOKEN="bkua_63fa9fec9d119f650e8b00a2b5f0c5c20036cc65"
 
 build_id="9053"
-cooldown_seconds=36  # 1 hour cooldown
+#cooldown_seconds=36  # 1 hour cooldown
 
 build_url="https://api.buildkite.com/v2/organizations/${ORG_NAME}/pipelines/${PIPELINE_NAME}/builds/${build_id}"
   
-response=$(curl -s -H "Authorization: Bearer ${API_TOKEN}" "${build_url}")
+response=$(curl -H "Authorization: Bearer $API_TOKEN" "$build_url")
