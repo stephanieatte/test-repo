a9f92923f5a430255fc24a84b1b1e82377affa5b
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2024-05-01 08:23:41 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -10,12 +10,10 @@ build_id="9053"
 
 # Function to check the status of the build
 check_build_status() {
-    build_url="https://api.buildkite.com/v2/organizations/$ORG_NAME/pipelines/$PIPELINE_NAME/builds/$build_id"
-    status=$(curl -H "Authorization: Bearer $API_TOKEN" -X GET "$build_url")
+    status=$(curl -H "Authorization: Bearer $API_TOKEN" -X GET "https://api.buildkite.com/v2/organizations/$ORG_NAME/pipelines/$PIPELINE_NAME/builds/$build_id")
     echo "$status"
     state=$(echo "$status" | jq -r '.state')
     echo "The Build state is $state"
 }
 
-# Example usage
-build_status=$(check_build_status)
+check_build_status
