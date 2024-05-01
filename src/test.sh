848e92b97055689c1fe600d09cb86314cf207e29
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2024-05-01 07:21:48 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -14,7 +14,6 @@ check_build_status() {
     wait_with_cooldown
     build_url="https://api.buildkite.com/v2/organizations/$ORG_NAME/pipelines/$PIPELINE_NAME/builds/$build_id"
     build_status=$(curl -s -H "Authorization: Bearer $API_TOKEN" "$build_url" | jq -r '.state')
-    echo "The Build state is: $build_status"
     if [ "$build_status" == "failed" ]; then
         echo "Build failed. Retrying after an hour..."
         trigger_build
@@ -34,7 +33,7 @@ trigger_build() {
 
 # Wait function with a cooldown
 wait_with_cooldown() {
-  sleep "${cooldown_seconds}"
+  sleep "$cooldown_seconds"
 }
 
 check_build_status
