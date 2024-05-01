fddf2cd32f1e4eec76ba85e812523c80f8e00884
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2024-05-01 06:54:15 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -14,15 +14,15 @@ check_build_status() {
     wait_with_cooldown
     build_url="https://api.buildkite.com/v2/organizations/$ORG_NAME/pipelines/$PIPELINE_NAME/builds/$build_id"
     build_status=$(curl -s -H "Authorization: Bearer $API_TOKEN" "$build_url" | jq -r ".state")
-    echo "The Build state is: ${build_status}"
+    echo "The Build state is: $build_status"
     
     if [ "$build_status" == "failed" ]; then
         echo "Build failed. Retrying after an hour..."
         trigger_build
-    elif [ "${build_status}" == "passed" ]; then
+    elif [ "$build_status" == "passed" ]; then
         echo "Build succeeded!"
     else
-        echo "Build status: ${build_status}."
+        echo "Build status: $build_status"
     fi
 }
 
@@ -41,4 +41,7 @@ wait_with_cooldown() {
 
 # Example usage
 build_status=$(check_build_status)
+
+
+
 
