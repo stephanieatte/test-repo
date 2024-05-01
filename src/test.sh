f86df9b345c68bd06fdc008a5c555e12dd580821
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2024-05-01 07:27:19 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -12,11 +12,10 @@ cooldown_seconds=10  # 1 hour cooldown
 # Function to check the status of the build
 check_build_status() {
     build_url="https://api.buildkite.com/v2/organizations/$ORG_NAME/pipelines/$PIPELINE_NAME/builds/$build_id"
-    build_status=$(curl -s -H "Authorization: Bearer $API_TOKEN" "$build_url" | jq -r ".state")
+    build_status=$(curl -s -H "Authorization: Bearer $API_TOKEN" -X GET "$build_url" | jq -r ".state")
     echo $build_status
 
 }
-
 
 
 # Wait function with a cooldown
