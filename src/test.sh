e19839135b635ca905716b35f55972d0d5179e9c
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2024-05-01 07:17:16 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -11,9 +11,20 @@ cooldown_seconds=10  # 1 hour cooldown
 
 # Function to check the status of the build
 check_build_status() {
-    build_url="https://api.buildkite.com/v2/organizations/atte-test-org-1/first/builds/9053"
-    build_status=$(curl -s -H "Authorization: Bearer bkua_63fa9fec9d119f650e8b00a2b5f0c5c20036cc65" "$build_url")
+    build_url="https://api.buildkite.com/v2/organizations/$ORG_NAME/pipelines/$PIPELINE_NAME/builds/$build_id"
+    build_status=$(curl -s -H "Authorization: Bearer $API_TOKEN" "$build_url")
     echo "$build_status"      
+    build_status=$(curl -s -H "Authorization: Bearer $API_TOKEN" "$build_url" | jq -r '.state')
+    echo "The Build state is: $build_status"
+     if [ "$build_status" == "failed" ]; then
+        echo "Build failed. Retrying after an hour..."
+        trigger_build
+    elif [ "$build_status" == "passed" ]; then
+        echo "Build succeeded!"
+    else
+        echo "Build status: $build_status"
+    fi
+
 }
 
 
